8c8dc193f70c95b4db1e44b5e86feddef5386458
Stiggins <derekpage1@gmail.com>
2019-11-16 20:18:55 -0500

    Added Buttons

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
-var colors = [];
+var startingColors = [];
 
 
-colors[0] = [51, 55, 69];
-colors[1] = [230, 52, 98];
-colors[2] = [254, 95, 85];
-colors[3] = [18, 78, 120];
+startingColors[0] = [51, 55, 69];
+startingColors[1] = [230, 52, 98];
+startingColors[2] = [254, 95, 85];
+startingColors[3] = [18, 78, 120];
+startingColors[4] = [238, 245, 219];
 
 var backgroundColor = "rgb(238, 245, 219)";
 
@@ -18,21 +19,84 @@ var multiples = 8;
 var frameTime = 13;
 var cycleColor = 0;
 var directionSize = 1;
+var colorsLength = 4;
+var limitMultiples = 16;
 var limitSize = 70;
-var colorsLength = colors.length;
-var limitColor = periodColor / (frameTime * colorsLength);
 var rateSize = (limitSize - width) * frameTime * 2 / periodSize;
 var countSize = width;
 var countColor = 0;
-var colorsLength = colors.length;
+var limitColor;
+var colors;
 
-for (i = colorsLength; i < colorsLength * multiples; i += colorsLength) {
-	for (j = 0; j < colorsLength; j++) colors[i + j] = colors[j];
+function colorFill() {
+	limitColor = periodColor / (frameTime * colorsLength);
+	colors = [];
+	for (i = 0; i < multiples * colorsLength; i += colorsLength) {
+		for (j = 0; j < colorsLength; j++) {
+			colors[i + j] = startingColors[j];
+		}
+	}
 }
+
+colorFill();
 
 var div = document.querySelector(".circle");
 var body = document.querySelector("body");
 body.style.background = backgroundColor;
+
+var colorsMinus = document.querySelector("#colors-minus");
+colorsMinus.addEventListener("click", function () {
+	if (colorsLength > 2) colorsLength--;
+	colorFill();
+
+	if (colorsLength === 2) {
+		colorsMinus.setAttribute("disabled", "")
+	}
+	if (colorsLength === startingColors.length - 1) {
+		colorsPlus.removeAttribute("disabled")
+	}
+})
+
+var colorsPlus = document.querySelector("#colors-plus");
+colorsPlus.addEventListener("click", function () {
+	if (colorsLength < startingColors.length) colorsLength++;
+	colorFill();
+
+	if (colorsLength === startingColors.length) {
+		colorsPlus.setAttribute("disabled", "")
+	}
+	if (colorsLength === 3) {
+		colorsMinus.removeAttribute("disabled")
+	}
+})
+
+
+var repeatsMinus = document.querySelector("#repeats-minus");
+repeatsMinus.addEventListener("click", function () {
+	if (multiples > 1) multiples--;
+	colorFill();
+
+	if (multiples === 1) {
+		repeatsMinus.setAttribute("disabled", "")
+	}
+	if (multiples === limitMultiples - 1) {
+		repeatsPlus.removeAttribute("disabled")
+	}
+})
+
+var repeatsPlus = document.querySelector("#repeats-plus");
+repeatsPlus.addEventListener("click", function () {
+	if (multiples < limitMultiples) multiples++;
+	colorFill();
+
+	if (multiples === limitMultiples) {
+		repeatsPlus.setAttribute("disabled", "")
+	}
+	if (multiples === 2) {
+		repeatsMinus.removeAttribute("disabled")
+	}
+})
+
 
 setInterval(function () {
 
@@ -41,8 +105,8 @@ setInterval(function () {
 
 	for (var i = 0; i < colors.length; i++) {
 		var iCalc = i + cycleColor;
-		if (iCalc >= colors.length) iCalc -= colors.length;
-		rgbCalc = [];
+		if (iCalc >= colors.length) iCalc = iCalc % colors.length;
+		var rgbCalc = [];
 		if (iCalc + 1 < colors.length) {
 			for (var j = 0; j < 3; j++) {
 				rgbCalc[j] = colors[iCalc][j] + (colors[iCalc + 1][j] - colors[iCalc][j]) * shft;
